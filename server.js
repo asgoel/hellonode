e68372fc9b81e86b58d26149d530df2def0de749
Ashu Goel <soxfan981@gmail.com>
2013-01-21 16:43:05 -0500

    finished tutorial

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route, handle) {
     function onRequest(request, response) {
-        console.log("Request received");
-        response.writeHead("200", {"Content-Type": "text/plain"});
-        response.write("Hello World!");
-        response.end();
+        var postData = "";
+        var pathname = url.parse(request.url).pathname
+        console.log("Request for " + pathname + " received");
+        route(handle, pathname, response, request);
     }
 
     http.createServer(onRequest).listen(8888);
